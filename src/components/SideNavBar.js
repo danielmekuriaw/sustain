5bb0d998920627fa61916fe02d84d482a6437531
Juliette Garcia <juliegarcia084@gmail.com>
2023-09-17 08:28:09 -0400

    basic react

---
+++ src/components/SideNavBar.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import AppTopBar from "./AppTopBar.js";
+import InfoCard from "./InfoCard.js";
+import InfoGrid from "./InfoGrid.js";
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Home', 'Analytics', 'Points', 'Profile'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default'}}
+      >
+          <div>
+        <AppTopBar/>
+          </div>
+
+
+        <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
+
+          <Typography variant="h3">
+            Analytics
+          </Typography>
+
+          <Box
+            component="main"
+            sx={{ bgcolor: 'background.default', p: 3 }}
+          >
+            <div>
+            <InfoGrid/>
+            </div>
+          </Box>
+
+
+        </Box>
+      </Box>
+    </Box>
+  );
+}
